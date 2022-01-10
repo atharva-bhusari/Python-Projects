1642b45b8e343b3011b6a0c57333f320e8200fd7
buzzi0204 <buzzi0204@gmail.com>
2022-01-10 15:57:08 +0530

    Day 32

---
+++ Birthday-wisher/main.py
@@ -0,0 +1,30 @@
+##################### Normal Starting Project ######################
+from datetime import datetime as dt
+import pandas as pd
+import random as rd
+import smtplib as stp
+
+MY_EMAIL = "TEST Email id"
+MY_PASSWORD = "Test password"
+
+today = (dt.now().month, dt.now().day)
+
+data = pd.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{rd.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with stp.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday\n\n{contents}")
+
+
---
+++ Motivational-Quotes-Sender/main.py
@@ -0,0 +1,23 @@
+import smtplib
+import datetime as dt
+import random as rd
+
+MY_EMAIL = "Test email"
+MY_PASSWORD ="test password"
+
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = rd.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject: Monday Motivation\n\n{quote}"
+        )
